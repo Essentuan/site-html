ba09247e1bd95b2ac201fa17aaebd649aa3113fd
Essentuan <51426319+Essentuan@users.noreply.github.com>
2019-07-04 20:32:50 -0500

    Update

---
+++ chores/database.js
@@ -0,0 +1,60 @@
+// Your web app's Firebase configuration
+let firebaseConfig = {
+  apiKey: "AIzaSyAmHSuQJYzvtYS2A9XDClOXEG8AmTYPlcg",
+  authDomain: "chore-tracker-d1f1a.firebaseapp.com",
+  databaseURL: "https://chore-tracker-d1f1a.firebaseio.com",
+  projectId: "chore-tracker-d1f1a",
+  storageBucket: "",
+  messagingSenderId: "753280324287",
+  appId: "1:753280324287:web:5056f377699f269a"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+let data = firebase.firestore()
+function addData(username, password) {
+  let newData = {
+    ps: password
+  }
+  var docRef = data.collection("LoginDetails").doc(username);
+  docRef.get().then(function(doc) {
+      if (doc.exists) {
+        M.toast({html: "A user with that username already exists"});
+      } else {
+        data.collection("LoginDetails").doc(username).set(newData);
+        M.toast({html: "Account Created"});
+      }
+    }).catch(function(error) {
+         M.toast({html: "An error has occured, please try again later"});
+    });
+}
+function login(username, password) {
+  var docRef = data.collection("LoginDetails").doc(username);
+  docRef.get().then(function(doc) {
+      if (doc.exists) {
+          if (doc.data().ps == password) {
+            M.toast({html: "<span>Logged in as " + username + "</span>"});
+            switchMain()
+          } else {
+            M.toast({html: "<span>Incorrect password</span>"});
+          }
+      } else {
+        let createA = '<span>User not found, are you sure you dont want to create an account?</span><button id="createA" class="btn-flat toast-action" style="color: white;">Create</button>';
+        M.toast({html: createA});
+      }
+    }).catch(function(error) {
+         M.toast({html: "An error has occured, please try again later"});
+    });
+}
+function switchCreate() {
+  $("main").empty();
+  $("main").load("html/create.html"); 
+}
+function switchLogin() {
+  $("main").empty();
+  $("main").load("html/login.html"); 
+}       
+function switchMain() {
+  $("main").empty();
+  $("main").load("html/main.html"); 
+  $("#background").width(0)
+}
